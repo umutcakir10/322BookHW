cb509b1d75aa0777a154c9ac058e3e980cc37201
umutcakir10 <ucakir81@gmail.com>
2024-02-27 21:38:35 +0300

    HW1

---
+++ 322Homework1/322Homework1/Controllers/BookController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Mvc;
+using _322Homework1.Models;
+
+
+namespace _322Homework1.Controllers
+{
+ 
+
+
+
+
+    public class BookController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+
+        }
+        public IActionResult Display(string name, string author, int page_number, DateTime due_date, int a)
+        {
+            Book book = new Book();
+            book.name = name;
+            book.author = author;
+            book.page_number = page_number;
+            book.due_date = due_date;
+            ///book.remainingTime = remainingTime;
+            DateTime now = DateTime.Now;
+            ///remainingTime = due_date - now;
+
+            TimeSpan remainingTime = due_date - now;
+            ///a = remainingTime.TotalDays;
+            book.remainingTime = remainingTime;
+            return View(book);
+        }
+    }
+
+}
---
+++ 322Homework1/322Homework1/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+
+namespace _322Homework1.Models
+{
+    public class Book
+    {
+        public Book()
+        {
+
+        }
+        public string name { get; set; }
+        public string author { get; set; }
+        public int page_number { get; set; }
+        public DateTime due_date { get; set; }
+        public TimeSpan remainingTime { get; set; }
+    }
+}
